59b58078ab6c7b520309dc3fa0c5be45a515b07c
Matheus Pires <matheushenriquepires99@gmail.com>
2020-05-15 11:11:16 -0300

    Ajusting user routes return

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,5 +1,4 @@
 import { Router } from "express";
-import request from "request";
 
 import api from "../services/api";
 
@@ -8,7 +7,17 @@ const userRouter = Router();
 userRouter.get("/", async (req, res) => {
   const response = await api.get("/me");
 
-  return res.json(response.data);
+  const { id, type, display_name, email, images } = response.data;
+
+  const user = {
+    id,
+    type,
+    display_name,
+    email,
+    avatar: images[0].url,
+  };
+
+  return res.json(user);
 });
 
 export default userRouter;
